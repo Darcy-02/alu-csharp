f387517b22fc393433f88ec13d67a01d52e3ce15
Blondie-02 <mbanzad14@gmail.com>
2026-05-11 04:41:41 -0700

    all projects remaining

---
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(
+            string.Concat("object", "-", "oriented", " ", "programming", " ", "in", " ", "C#")
+        );
+    }
+}
+// Output: object-oriented programming in C#
+
