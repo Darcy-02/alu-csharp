de54bbdfc2d198a48f4df12b53eaa496caae3833
Blondie-02 <mbanzad14@gmail.com>
2026-05-11 04:57:42 -0700

    edited project 11

--- csharp-hello_world/11-concat_edges/11-concat_edges.cs
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -1,12 +1,11 @@
 ﻿using System;
+
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine(
-            string.Concat("object", "-", "oriented", " ", "programming", " ", "in", " ", "C#")
-        );
-    }
+	static void Main(string[] args)
+	{
+		string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers.";
+		string.Concat("object", "-", "oriented", " ", "programming", " ", "in", "
+		Console.WriteLine(str);
+        }
 }
-// Output: object-oriented programming in C#
-
