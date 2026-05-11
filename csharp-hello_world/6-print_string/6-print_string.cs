a0ae8a344713e3c068f8389e57d7c3433e8c3c7b
Blondie-02 <mbanzad14@gmail.com>
2026-05-11 04:43:40 -0700

    all projects remaining

---
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str = "Holberton School";  // Declare and initialize the string variable
+
+        // Print the string 3 times
+        Console.WriteLine(str + str + str);
+
+        // Print the first 9 characters of the string
+        Console.WriteLine(str.Substring(0, 9));
+    }
+}
